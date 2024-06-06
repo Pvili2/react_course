d34b49165c0a6c1920c7b252bdac11cca55ac1a1
Padla Vili <pvilmos2003@gmail.com>
2024-06-06 21:28:40 +0200

    fix

---
+++ Section 14/src/components/Users.js
@@ -0,0 +1,48 @@
+import { useState, Component } from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error("No users Provided");
+    }
+  }
+  toggleUsersHandler() {
+    this.setState({ showUsers: !this.state.showUsers })
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+export default Users;
